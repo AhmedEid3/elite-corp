9d208203cb0f96b659d942923cef031f6233bd75
AhmedEid3 <ahmed.eid3@outlook.com>
2017-11-04 19:37:51 +0200

    add files

--- js/main.js
+++ js/main.js
@@ -8,4 +8,16 @@ $(function(){
 
     $('.slider, .carousel-item').height( winH - (upperNavH + navbarH));
 
+    // Start Featured Work shuffle-images
+    $('.featured-work ul li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        if( $(this).data('class') === 'all' ) {
+            $('.featured-work .shuffle-images img').css('opacity', 1);
+        }
+        else {
+            $('.featured-work .shuffle-images img').css('opacity', 0.1);
+            $($(this).data('class')).css('opacity', 1);
+        }
+    });
+
 });
